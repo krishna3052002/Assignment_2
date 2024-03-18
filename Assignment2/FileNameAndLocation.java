b18ec8898d8d1f4a4e517a3cc28b115b42747d2f
krishna3052002 <shukrishna23@gmail.com>
2024-03-19 00:29:19 +0600

    First Commit

---
+++ Assignment2/FileNameAndLocation.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class FileNameAndLocation {
+
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.printf("Please enter the Path:");
+        String fileName=sc.next();
+        System.out.printf("%s\n",fileName);
+//        System.out.printf("C:\\Windows\\System\\Readme.txt");
+
+    }
+
+
+}
